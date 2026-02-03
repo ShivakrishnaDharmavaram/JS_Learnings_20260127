a7230b522cee35a963970eb03115202efff4d05f
skdharmavaram <dharmavaram.shivakrishna@zensar.com>
2026-02-03 17:43:42 +0530

    forEach

---
+++ 01_JS_Basics/10_map_filter_reduce_foreach/03_foreach.js
@@ -0,0 +1,21 @@
+// .forEach() is used to iterate over each element in an array 
+// and perform an action, but unlike map(), it does not return a new array. 
+
+const numbers = [1, 2, 3, 4, 5];
+
+// Using forEach to print each number
+numbers.forEach(num => {
+    console.log(num);
+});
+
+// print square of each number
+numbers.forEach(num => {
+    console.log(num * num);
+});
+
+// challenge: forEach to calculate the sum of numbers
+let sum = 0;
+numbers.forEach(num => {
+    sum += num;
+})
+console.log("Sum of Numbers:", sum);
